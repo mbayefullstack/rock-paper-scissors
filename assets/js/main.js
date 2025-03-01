b27c3f9ae78e0435d234dc707fcbf49deafabfee
Mouhamadou Moustapha MBAYE <mmbaye11@gmail.com>
2025-03-01 16:39:22 +0000

    Subject: Implement the human choice.
    Body:
    - Create a function that get user imput via prompt and return that.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+//Get the computer choice.
+
 function getComputerChoice() {
     let choice = "";
     const choiceComputer = Math.floor(Math.random() * 3) + 1;
@@ -12,4 +14,26 @@ function getComputerChoice() {
     return choice;
 }
 
-alert(getComputerChoice())
+
+// Get the user choice.
+
+function getHumanChoice() {
+    let promptHuman = prompt("What's your choice?");
+    promptHuman = promptHuman.toLocaleLowerCase()
+    if(promptHuman === "") {
+        alert("Can you write a good choice?")
+        promptHuman = prompt("What's your choice?");
+    } else if(promptHuman == "Rock".toLowerCase()) {
+        promptHuman = "Rock";
+    } else if(promptHuman == "Paper".toLowerCase()) {
+        promptHuman = "Paper";
+    } else if (promptHuman == "Scissors".toLowerCase()) {
+        promptHuman = "Scissors";
+    } else {
+        alert("Can you write a good choice?");
+        promptHuman = prompt("What's your choice?");
+    }
+    return promptHuman;
+
+}
+console.log(getHumanChoice())
